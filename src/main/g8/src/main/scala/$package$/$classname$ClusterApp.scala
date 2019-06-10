19b749139e1cf81684f7f7768cc0d2f4188ee20b
Greg Maslowski <grzegorz.maslowski@ista.com>
2019-06-10 10:08:17 +0200

    2 seed nodes, but 3 in total.

--- src/main/g8/src/main/scala/$package$/$classname$ClusterApp.scala
+++ src/main/g8/src/main/scala/$package$/$classname$ClusterApp.scala
@@ -3,6 +3,7 @@ package $package$
 object $classname$ClusterApp
   extends App {
 
-  $classname$App.main(Seq("2018").toArray)
-  $classname$App.main(Seq("2019").toArray)
+  $classname$App.main(Seq("2552").toArray)
+  $classname$App.main(Seq("2553").toArray)
+  $classname$App.main(Seq("2554").toArray)
 }
